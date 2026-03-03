232031adbdf1a1a4dc2e4673c281afbdff57771d
Albert <albert.corzo.g@gmail.com>
2026-03-03 11:42:07 +0100

    chore: bump version to 2.3.2

--- bugtrace/core/config.py
+++ bugtrace/core/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
     """
     # --- Project Metadata ---
     APP_NAME: str = "BugtraceAI-CLI"
-    VERSION: str = "2.3.1"  # Phoenix Edition
+    VERSION: str = "2.3.2"  # Phoenix Edition
     DEBUG: bool = False
     SAFE_MODE: bool = False # Default to False, override via CLI
 
